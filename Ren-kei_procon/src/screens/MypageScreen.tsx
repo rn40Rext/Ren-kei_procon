da4b742820a2c5c71f1595ea1f40762fcd712932
Sari-take <sata.quality02@gmail.com>
2026-08-19 09:33:31 +0900

    マイページを更新

--- Ren-kei_procon/src/screens/MypageScreen.tsx
+++ Ren-kei_procon/src/screens/MypageScreen.tsx
@@ -1,12 +1,67 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import BottomNav from '../components/BottomNav';
 
-export default function AnalysisScreen() {
+export default function MypageScreen() {
   return (
-    <View style={{ flex: 1 }}>
-      <Text>マイページ</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.profile}>
+
+        <Text style={styles.title}>マイページ</Text>
+
+        <Text style={styles.normal}>名前：</Text>
+        <Text style={styles.normal}>連：</Text>
+        <Text style={styles.normal}>自己紹介</Text>
+
+        <TouchableOpacity style={styles.editButton}>
+          <Text style={styles.editButtonText}>プロフィールを編集</Text>
+        </TouchableOpacity>
+
+        </View>
+      </ScrollView>
 
       <BottomNav />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f9fafb',
+    padding: 20,
+  },
+
+  profile: {
+    margin: 20,
+    padding: 20,
+    backgroundColor: '#ffffff',
+    borderRadius: 12,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+
+  normal: {
+    fontSize: 16,
+    marginBottom: 12,
+  },
+
+  editButton: {
+    backgroundColor: '#2563eb',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignSelf: 'flex-start',
+    marginBottom: 15,
+  },
+
+  editButtonText: {
+    color: '#ffffff',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+})
